7105cef10ce95bff834d8d98cf32aa3610ce8f84
tonybanters <tonyoutoften@gmail.com>
2022-03-19 18:27:01 -0700

    Added doc strings for functions, and removed random aux lines.

--- src/project-euler/even-Fibonacci-numbers/index.ts
+++ src/project-euler/even-Fibonacci-numbers/index.ts
@@ -1,4 +1,7 @@
-
+/**
+ * This IIFE function initializes a memo for fib numbers, and sets fib equal to
+ * fibonacci(n) which returns the nth fibonacci number using recursion, and memoization.
+ */
 const fib: Function = (() => {
   
   type Memo = {[key: number]: number}
@@ -6,20 +9,26 @@ const fib: Function = (() => {
     1 : 1,
     2 : 1
   }
-
+  /**
+   * This function returns the nth fibonacci number.
+   * @param n 
+   * @returns 
+   */
   const fibonacci = (n: number): number => {
     if (n in memo) return memo[n];
-
-    if (n <= 1) {
-      return 1
-    }
-    else {
-      return memo[n] = fibonacci(n-2) + fibonacci(n-1)
-    }
+    if (n <= 1) return 1;
+    
+    return memo[n] = fibonacci(n-2) + fibonacci(n-1)
   }
   return fibonacci;
 })();
 
+/**
+ * This function takes an integer n as input, and returns the sum of all even
+ * fibonacci numbers that are less than n.
+ * @param n 
+ * @returns 
+ */
 const evenFibSum = (n: number): number => {
   let sum = 0;
   let temp = 1;
@@ -33,8 +42,5 @@ const evenFibSum = (n: number): number => {
   }
   return sum
 }
-
-
-
 
 export default evenFibSum;
